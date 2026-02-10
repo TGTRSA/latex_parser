959ca6af748788a036646d22fd0cf4f17d80b3cf
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-02-10 17:16:35 +0000

    [FIRST] Created src folder for various resources and the structure of a function to read the source file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <string>
+
+
+
+std::string read_file() {
+    std::string file_contents;
+    return file_contents;
+}
+
+
+int main(int argc, char **argv) {
+    printf("Starting program\n");
+    printf("The name of the file is %s with %d number of chars", argv[1], argc);
+
+    return 0;
+}
