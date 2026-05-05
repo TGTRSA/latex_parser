cc762d7ae7a1ece2730a8944e6e02677996a88ed
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-05-05 18:47:14 +0000

    [parser.h + parser.c] Base function to write header to latex doc string

--- parser.c
+++ parser.c
@@ -1,7 +1,45 @@
 #include "parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* latex_code = "\\documentclass{article}\n"
+                    "\\usepackage[utf8]{inputenc}\n"
+                    "\\usepackage{asmath}\n"
+                    "\\usepackage{tikz}\n"
+                    "\\usepackage{geometry}";
+
+const char* begin_doc  = "\n\\begin{document}\n";
+const char* end_doc    = "\\end{document}\n";
+bool header_written=false;
+
+void write_header(Token *t) {
+    printf("%s\n", t->data);
+    
+    char* usepackage = "\\usepackage{}";
+    char* buf = (char *)  malloc(  sizeof(char) * strlen(latex_code));
+    size_t doc_opening_len = sizeof(char) * strlen(usepackage) + strlen(t->data) + strlen(latex_code);
+    char* full_tex = (char *) (malloc(doc_opening_len )); 
+    char* buffer = (char *) malloc(sizeof(char)*(strlen(usepackage)+strlen(t->data)));
+    
+    // taking the latex content and putting it into a buffer that can handle it 
+    // then creating the psckage and appending it to the buffer
+    strcpy(full_tex,latex_code);
+    // free(latex_code);
+    sprintf(buffer,  "\n\\usepackage{%s}", t->data);
+    strcat(full_tex,buffer);
+
+    latex_code = (char *) malloc(sizeof(char) * strlen(full_tex) + 1);
+
+    free(buffer);
+    latex_code = full_tex;
+    printf("%s\n", latex_code);
+
+    // free(full_tex);
+}
 
 void compile_tex(token_container *container){
-    printf("[DEBUG] \nIn parser.h:\n");
+    printf("\n[DEBUG] In parser.h:\n");
     size_t i;    
     for(i=0;i<container->length;i++){
         Token t = container->tokens[i] ;
@@ -9,6 +47,8 @@ void compile_tex(token_container *container){
             case HEADER:
             {
                 printf("HEADER(%s)\n",t.data);
+                write_header( &t);
+                printf("%s\n",latex_code);
                 break;
             }
             case INLINE_EQ:{
--- parser.h
+++ parser.h
@@ -1,8 +1,21 @@
 #ifndef PARSER
 #define PARSER
 #include "lexer.h"
+#include <stdlib.h>
 #pragma once
-
+//return (
+        //     "\\documentclass{article}\n"
+        //     "\\usepackage[utf8]{inputenc}\n"
+        //     "\\usepackage{amsmath}\n"
+        //     "\\usepackage{amssymb}\n"
+        //     "\\usepackage{tikz}\n"
+        //     "\\usepackage{pgfplots}\n"
+        //     "\\usepackage{chemfig}"
+        //     "\\usepackage{geometry}\n"
+        //     "\\begin{document}\n"
+        //     f"{body}\n"
+        //     "\\end{document}"
+        // )
 
 
 void compile_tex(token_container* tokens);
